cbeeb3beba4a4834e4fa79434e0a708882270408
bao <danglehuubaophc@gmail.com>
2022-11-03 12:27:00 +0700

    ASD

--- controllers/api/page.js
+++ controllers/api/page.js
@@ -10,7 +10,10 @@ const loadmoremedia = require('../../middlewares/loadmore_media');
 const getdata = require('../../middlewares/getdata_post_page');
 const autoScroll_post = require('../../middlewares/autoscrollpost');
 const genSlug = require('../../middlewares/genslug');
-
+const Post = require('../../models/post');
+const Post_detail = require('../../models/post_detail');
+const Page = require('../../models/page');
+const Posttype = require('../../models/posttype');
 require('dotenv').config();
 
 //const bigquery = require('./bigquery');
@@ -48,12 +51,22 @@ async function autoScroll(page, lengthss, like, comment, share) {
             clearInterval(timer);
             resolve();
           }
-          let length_feed = document.querySelectorAll('[role="feed"]');
-          document.querySelectorAll('[role="feed"]')[length_feed.length - 1].childNodes.forEach((item) => {
-            if (item.className !== '' && item.nodeName == 'DIV') {
-              length_post += 1;
-            }
+          document.querySelectorAll('div').forEach((el) => {
+            el.childNodes.forEach((eel) => {
+              if (eel.nodeName == '#comment') {
+                eel.remove();
+              }
+            });
           });
+          //let length_feeds = document.querySelectorAll('[data-pagelet="ProfileTimeline""]')[0];
+          let length_feed =
+            document.querySelectorAll('[role="main"]')[0].childNodes[3].childNodes[1].childNodes[0].childNodes[1]
+              .childNodes[1].childNodes.length;
+          // document.querySelectorAll('[role="feed"]')[length_feed.length - 1].childNodes.forEach((item) => {
+          //   if (item.className !== '' && item.nodeName == 'DIV') {
+          //     length_post += 1;
+          //   }
+          // });
           let isbottom = document.body.scrollHeight;
           let istop = parseInt(document.documentElement.scrollTop + window.innerHeight);
           if (isbottom === istop) {
@@ -61,8 +74,8 @@ async function autoScroll(page, lengthss, like, comment, share) {
             resolve();
           }
 
-          console.log(length_post);
-          if (length_post - 5 >= lengthss) {
+          //console.log(length_post);
+          if (length_feed - 5 >= lengthss) {
             clearInterval(timer);
             resolve();
           }
@@ -97,8 +110,23 @@ module.exports = async function main(req) {
     const share = req.data.data.share ? req.data.data.share : 0;
     const post_type = req.data.data.posttype ? req.data.data.posttype : '';
     const craw_id = crypto.randomBytes(16).toString('hex');
-    const app = initializeApp.initializeApp(firebaseConfig);
-    const database = getDatabase(app);
+    let page_id = '';
+    let Posttype_id = '';
+
+    Posttype.findOne({ url: url }, async function (err, posttype) {
+      if (posttype) {
+        group_id = posttype._id;
+      } else {
+        let Posttypes = new Posttype({
+          name: post_type,
+          create_at: new Date(),
+        });
+        await Posttypes.save();
+        Posttype_id = Posttypes._id;
+      }
+    });
+    // const app = initializeApp.initializeApp(firebaseConfig);
+    // const database = getDatabase(app);
     // const app = initializeApp.initializeApp(firebaseConfig);
     // const database = getDatabase(app);
     // const postListRefs = ref(
@@ -116,6 +144,7 @@ module.exports = async function main(req) {
     //   url: url,
     //   create_at: Date.now(),
     // });
+
     const browser = await puppeteer.launch({
       ignoreHTTPSErrors: true,
       ignoreDefaultArgs: ['--disable-extensions'],
@@ -155,6 +184,10 @@ module.exports = async function main(req) {
       executablePath: 'C:/Program Files/Google/Chrome/Application/chrome.exe', // windows
       //executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome', // MacOS
     });
+    const context = browser.defaultBrowserContext();
+    //        URL                  An array of permissions
+    context.overridePermissions('https://www.facebook.com', ['geolocation', 'notifications']);
+
     const browser2 = await puppeteer.launch({
       ignoreHTTPSErrors: true,
       ignoreDefaultArgs: ['--disable-extensions'],
@@ -227,17 +260,37 @@ module.exports = async function main(req) {
 
       //await new Promise((r) => setTimeout(r, 4000));
       await page.waitForSelector('div', { hidden: true });
+      await page.waitFor(1000);
+      await page.waitForSelector('#pagelet_composer');
+      let content2 = await page.$$('#pagelet_composer');
+
       await page.goto(url, {
         waitUntil: 'load',
       });
       //aaa
-      //await page.waitForFunction('document.querySelector("h2")[0]');
+      await page.waitForFunction('document.querySelector("h1")');
     } catch (e) {
       console.log(e);
     }
-    // const result = await page.evaluate(() => {
-    //   return document.querySelector('h2')[0].textContent;
-    // });
+    const result = await page.evaluate(() => {
+      return document.querySelectorAll('h1')[1]
+        ? document.querySelectorAll('h1')[1].textContent
+        : document.querySelectorAll('h1')[0].textContent;
+    });
+    Page.findOne({ url: url }, async function (err, page) {
+      if (page) {
+        page_id = page._id;
+      } else {
+        let pages = new Page({
+          name: result,
+          url: url,
+          create_at: new Date(),
+        });
+        await pages.save();
+        group_id = pages._id;
+      }
+    });
+
     await page.waitForSelector('h2', { visible: true });
 
     // const postListRefss = ref(databases, 'Group/' + name.replace(/[#:.,$]/g, ''));
@@ -256,32 +309,32 @@ module.exports = async function main(req) {
     // });
 
     await getlink(page, conten_length, like, comment, share).then(async function (result) {
-      const craw_link_jobId = ref(database, 'ListLink_jobId/' + req.data.jobId + '/');
-      await set(craw_link_jobId, {
-        name: result,
-        craw_id: req.data.jobId,
-        url: req.data.data.link,
-        lengths: req.data.data.count == '' ? 0 : req.data.data.count,
-        cmt_length: req.data.data.length_comment == '' ? 0 : req.data.data.length_comment,
-        conten_length: req.data.data.length_content == '' ? 0 : req.data.data.length_content,
-        like: req.data.data.like ? req.data.data.like : 0,
-        comment: req.data.data.comment ? req.data.data.comment : 0,
-        share: req.data.data.share ? req.data.data.share : 0,
-        post_type: req.data.data.posttype ? req.data.data.posttype : '',
-      });
-      const craw_link_crawid = ref(database, 'ListLink_crawid/' + craw_id + '/');
-      await set(craw_link_crawid, {
-        name: result,
-        craw_id: req.data.jobId,
-        url: req.data.data.link,
-        lengths: req.data.data.count == '' ? 0 : req.data.data.count,
-        cmt_length: req.data.data.length_comment == '' ? 0 : req.data.data.length_comment,
-        conten_length: req.data.data.length_content == '' ? 0 : req.data.data.length_content,
-        like: req.data.data.like ? req.data.data.like : 0,
-        comment: req.data.data.comment ? req.data.data.comment : 0,
-        share: req.data.data.share ? req.data.data.share : 0,
-        post_type: req.data.data.posttype ? req.data.data.posttype : '',
-      });
+      // const craw_link_jobId = ref(database, 'ListLink_jobId/' + req.data.jobId + '/');
+      // await set(craw_link_jobId, {
+      //   name: result,
+      //   craw_id: req.data.jobId,
+      //   url: req.data.data.link,
+      //   lengths: req.data.data.count == '' ? 0 : req.data.data.count,
+      //   cmt_length: req.data.data.length_comment == '' ? 0 : req.data.data.length_comment,
+      //   conten_length: req.data.data.length_content == '' ? 0 : req.data.data.length_content,
+      //   like: req.data.data.like ? req.data.data.like : 0,
+      //   comment: req.data.data.comment ? req.data.data.comment : 0,
+      //   share: req.data.data.share ? req.data.data.share : 0,
+      //   post_type: req.data.data.posttype ? req.data.data.posttype : '',
+      // });
+      // const craw_link_crawid = ref(database, 'ListLink_crawid/' + craw_id + '/');
+      // await set(craw_link_crawid, {
+      //   name: result,
+      //   craw_id: req.data.jobId,
+      //   url: req.data.data.link,
+      //   lengths: req.data.data.count == '' ? 0 : req.data.data.count,
+      //   cmt_length: req.data.data.length_comment == '' ? 0 : req.data.data.length_comment,
+      //   conten_length: req.data.data.length_content == '' ? 0 : req.data.data.length_content,
+      //   like: req.data.data.like ? req.data.data.like : 0,
+      //   comment: req.data.data.comment ? req.data.data.comment : 0,
+      //   share: req.data.data.share ? req.data.data.share : 0,
+      //   post_type: req.data.data.posttype ? req.data.data.posttype : '',
+      // });
 
       let process = 0;
       await browser.close();
@@ -354,33 +407,28 @@ module.exports = async function main(req) {
               if (err) throw err;
               console.log('The file has been saved!');
             });
-            let results = '';
-            console.log(data);
+            let results = data;
             if (parseInt(data.imagemore) > 0) {
               results = await loadmoremedia(page1, data);
-              console.log(results);
-            } else {
-              results = data;
             }
-
             if (!results.ismain || !results.iscate || !results.iscontent || !results.isuser) {
-              const error = ref(
-                databases,
-                'Error/' + name.replace(/[#:.,$]/g, '') + '/' + result[i].post_link.split('/')[5]
-              );
-              await set(error, {
-                name: result[i].post_link,
-                ismain: results.ismain,
-                iscate: results.iscate,
-                isuser: results.isuser,
-                iscontent: results.iscontent,
-              });
+              // const error = ref(
+              //   databases,
+              //   'Error/' + name.replace(/[#:.,$]/g, '') + '/' + result[i].post_link.split('/')[5]
+              // );
+              // await set(error, {
+              //   name: result[i].post_link,
+              //   ismain: results.ismain,
+              //   iscate: results.iscate,
+              //   isuser: results.isuser,
+              //   iscontent: results.iscontent,
+              // });
               return;
             }
-            const postListRef = ref(
-              database,
-              'post_type/' + post_type + '/' + name.replace(/[#:.,$]/g, '') + '/' + result[i].post_link.split('/')[5]
-            );
+            // const postListRef = ref(
+            //   database,
+            //   'post_type/' + post_type + '/' + name.replace(/[#:.,$]/g, '') + '/' + result[i].post_link.split('/')[5]
+            // );
             let titles = '';
             let short_descriptions = '';
             // let arrVid = null;
@@ -542,11 +590,97 @@ module.exports = async function main(req) {
                 : [],
             };
             //await bigquery(basic_fields, custom_fields);
+            try {
+              let post = new Post({
+                basic_fields: JSON.stringify(basic_fields, null, 2),
+                custom_fields: JSON.stringify(custom_fields, null, 2),
+                group_id: group_id,
+                posttype: Posttype_id,
+              });
+              let postdetail = new Post_detail({
+                group_id: group_id,
+                posttype: Posttype_id,
+                title: titles,
+                short_description: short_descriptions,
+                long_description: results.contentList
+                  ? results.contentList.replaceAll('https://l.facebook.com/l.php?', '')
+                  : '',
+                slug: '',
+                featured_image: results.linkImgs[0] ? results.linkImgs[0] : '',
+                session_tags: {
+                  tags: [],
+                },
+                categorialue: [],
+                key: '',
+                name: '',
+                type: post_type,
+                attributes: [],
+                is_active: 1,
+                status: 'publish',
+                seo_tags: {
+                  meta_title: 'New Post Facebook',
+                  meta_description: 'New Post Facebook',
+                },
+                video: results.videos,
+                date: results.date ? results.date : '',
+                post_id: results.idPost ? results.idPost : '',
+                post_link: result[i].post_link ? result[i].post_link : '',
+                user_id: results.user_id ? results.user_id : 'undefined',
+                user_name: results.user ? results.user : 'undefined',
+                count_like: results.countLike
+                  ? results.countLike.toString().split(' ')[0].indexOf(',') > -1
+                    ? parseInt(results.countLike.toString().split(' ')[0].replace('K', '00').replace(',', ''))
+                    : parseInt(results.countLike.toString().split(' ')[0].replace('K', '000'))
+                  : 0,
+                count_comment: results.countComment
+                  ? results.countComment.toString().split(' ')[0].indexOf(',') > -1
+                    ? parseInt(results.countComment.toString().split(' ')[0].replace('K', '00').replace(',', ''))
+                    : parseInt(results.countComment.toString().split(' ')[0].replace('K', '000'))
+                  : 0,
+                count_share: results.countShare
+                  ? results.countShare.toString().split(' ')[0].indexOf(',') > -1
+                    ? parseInt(results.countShare.toString().split(' ')[0].replace('K', '00').replace(',', ''))
+                    : parseInt(results.countShare.toString().split(' ')[0].replace('K', '000'))
+                  : 0,
+                featured_image: results.linkImgs ? results.linkImgs : '',
+                comments: results.commentList
+                  ? results.commentList.map((item) => ({
+                      content: item.contentComment,
+                      count_like: item.countLike
+                        ? item.countLike.toString().split(' ')[0].indexOf(',') > -1
+                          ? parseInt(item.countLike.toString().split(' ')[0].replace('K', '00').replace(',', ''))
+                          : parseInt(item.countLike.toString().split(' ')[0].replace('K', '000'))
+                        : 0,
+                      user_id: item.userIDComment,
+                      user_name: item.usernameComment,
+                      imgComment: item.imageComment ? item.imageComment : '',
+                      children: item.children
+                        ? item.children.map((child) => ({
+                            content: child.contentComment,
+                            count_like: child.countLike
+                              ? child.countLike.toString().split(' ')[0].indexOf(',') > -1
+                                ? parseInt(child.countLike.toString().split(' ')[0].replace('K', '00').replace(',', ''))
+                                : parseInt(child.countLike.toString().split(' ')[0].replace('K', '000'))
+                              : 0,
+                            user_id: child.userIDComment,
+                            user_name: child.usernameComment,
+                            imageComment: child.imageComment ? child.imageComment : '',
+                          }))
+                        : [],
+                    }))
+                  : [],
+              });
 
-            await set(postListRef, {
-              basic_fields: basic_fields,
-              custom_fields: custom_fields,
-            });
+              await post.save();
+              await postdetail.save();
+            } catch (e) {
+              console.log(e);
+            }
+
+            // await set(postListRef, {
+            //   basic_fields: basic_fields,
+            //   custom_fields: custom_fields,
+            // });
             // const postListRefss = ref(
             //   database,
             //   '/Listpost/' + name.replace(/[#:.,$]/g, '') + '/' + url.split('/')[6]
@@ -589,14 +723,14 @@ module.exports = async function main(req) {
           console.log(e);
           console.log('lỗi error');
 
-          const postListRefss = ref(
-            database,
-            '/Listpost_error/' + name.replace(/[#:.,$]/g, '') + '/' + url.split('/')[6]
-          );
-          await set(postListRefss, {
-            post_link: url,
-            error: 'error' + e,
-          });
+          // const postListRefss = ref(
+          //   database,
+          //   '/Listpost_error/' + name.replace(/[#:.,$]/g, '') + '/' + url.split('/')[6]
+          // );
+          // await set(postListRefss, {
+          //   post_link: url,
+          //   error: 'error' + e,
+          // });
           // const postListRefs = ref(
           //   database,
           //   '/craw_list/' + name.replace(/[#:.,$]/g, '') + '/' + craw_id
