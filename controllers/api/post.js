79a252447d20d6e3e4c8a350dac2c83485f26afe
bao <danglehuubaophc@gmail.com>
2022-11-07 15:45:10 +0700

    adsad

--- controllers/api/post.js
+++ controllers/api/post.js
@@ -1,27 +1,59 @@
+const { async } = require('@firebase/util');
 const Post = require('../../models/post');
 const Posttype = require('../../models/posttype');
+const redis = require('redis');
+const { DEFAULT_INTERCEPT_RESOLUTION_PRIORITY } = require('puppeteer');
+
 class Postapi {
   async getPost(req, res) {
-    let page = parseInt(req.query.page);
-    let limit = parseInt(req.query.limit);
+    let redisClient = redis.createClient({
+      legacyMode: true,
+      socket: {
+        port: 6379,
+        host: '127.0.0.1',
+      },
+    });
+    await redisClient.connect();
+    // redisClient.get('bao', async (err, data) => {
+    //   if (err) console.log(err);
+    //   if (data != null) {
+    //     return res.json(JSON.parse(data));
+    //   } else {
+    let page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 100000000000000;
     let post_type = req.query.posttype;
+    let search = req.query?.search;
     let skip = (page - 1) * limit;
     if (post_type == undefined) {
-      res.json({ data: 'erroe', error: 'no posttype' });
+      res.json({ data: 'error', error: 'no posttype' });
       return;
     }
-
     let posttype_id = await Posttype.findOne({ name: post_type });
     if (posttype_id === null) {
       res.json({ data: 'error', error: 'no such posttype' });
       return;
-    }
-    let post = await Post.find({ posttype: posttype_id._id })
+    } //https://www.facebook.com/groups/j2team.community/posts/1980870165578427/
+    let post = await Post.find({ posttype: posttype_id._id, post_link: { $regex: search } })
       .sort([['create_at', -1]])
       .skip(skip)
-      .limit(limit);
+      .limit(limit)
+      .lean();
+    post.forEach(async (element, index) => {
+      delete element.post_link;
+    });
     let posts = await Post.find({ posttype: posttype_id._id }).count();
-    await res.json({ count_pages: parseInt(Math.ceil(posts / limit)), count_posts: parseInt(posts), data: post });
+    //await redisClient.set('bao', post);
+
+    await res.json({
+      count_pages: parseInt(Math.ceil(posts / limit)) || 1,
+      count_posts: parseInt(posts),
+      data: post,
+    });
+    // }
+    // });
+  }
+  async search(req, res, next) {
+    Post.find(req);
   }
   async getPost_id(req, res, next) {
     Post.findById(req.params.id, async function (err, post) {
